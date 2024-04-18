1e0cd170d5f23742f3c6d289031eb36b62b594e1
Sagar Maurya <147434812+asmigar@users.noreply.github.com>
2024-04-18 08:51:42 +0530

    create flask app

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,9 @@
-#!/usr/bin/python3
-print("Hello, World!")
+#!env/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World</p>"
+
